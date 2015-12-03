654373e5be24983f7092161148c2787671602143
Jun Isaji <jisaji@gmail.com>
2015-12-02 19:57:14 -0500

    We now check if the user exists in the database
    If they don't, we add them before adding the post

--- putPost/putPost.js
+++ putPost/putPost.js
@@ -4,8 +4,8 @@ var AWS = require("aws-sdk");
 var https = require('https');
 
 AWS.config.update({
-  region: "us-east-1",
-  endpoint: "https://dynamodb.us-east-1.amazonaws.com"
+    region: "us-east-1",
+    endpoint: "https://dynamodb.us-east-1.amazonaws.com"
 });
 
 var dynamodbDoc = new AWS.DynamoDB.DocumentClient();
@@ -17,22 +17,73 @@ exports.handler = function(event, context) {
         res.on('data', function (chunk) {
             console.log('BODY: ' + chunk);
             var userInfo = JSON.parse(chunk);
-            dynamodbDoc.put({
-                Item: {
-                    timestamp: Date.now(),
-                    userId: parseInt(userInfo.sub),
-                    postText: event.postText,
-                },
-                TableName : "postsV2",
-            }, function(err, data) {
+            //Check if user exists in database
+            dynamodbDoc.query({
+                TableName : "users",
+                KeyConditionExpression: "userId = :userId",
+                ExpressionAttributeValues: {
+                    ":userId": parseInt(userInfo.sub)
+                }
+            }, function (err, data) {
                 if (err) {
                     console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
                     context.fail();
                 } else {
-                    console.log("Put succeeded.");
-                    context.succeed(data);
+                    console.log("Query succeeded.");
+                    if (data.Items.length === 0) {
+                        //User not in table.
+                        dynamodbDoc.put({
+                            Item: {
+                                userId: parseInt(userInfo.sub),
+                                firstName: userInfo.given_name,
+                                lastName: userInfo.family_name
+                            },
+                            TableName : "users",
+                        }, function(err, data) {
+                            if (err) {
+                                console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+                                context.fail();
+                            } else {
+                                console.log("User Put succeeded.");
+                                //Put the post
+                                dynamodbDoc.put({
+                                    Item: {
+                                        timestamp: Date.now(),
+                                        userId: parseInt(userInfo.sub),
+                                        postText: event.postText,
+                                    },
+                                    TableName : "postsV2",
+                                }, function(err, data) {
+                                    if (err) {
+                                        console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+                                        context.fail();
+                                    } else {
+                                        console.log("Put succeeded.");
+                                        context.succeed(data);
+                                    }
+                                });
+                            }
+                        });
+                    } else {
+                        dynamodbDoc.put({
+                            Item: {
+                                timestamp: Date.now(),
+                                userId: parseInt(userInfo.sub),
+                                postText: event.postText,
+                            },
+                            TableName : "postsV2",
+                        }, function(err, data) {
+                            if (err) {
+                                console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+                                context.fail();
+                            } else {
+                                console.log("Put succeeded.");
+                                context.succeed(data);
+                            }
+                        });
+                    }
                 }
-            });     
+            });
         });
     }).on('error', function(e) {
         console.log("Got error: " + e.message);
